fcad66e30547674ffbde65f412e8f09d229b17da
Dustin Beckett <dustin.beckett23@gmail.com>
2020-11-14 12:51:28 -0500

    hello world

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import QFit from './src/app';
+import {name as appName} from './src/app.json';
+
+AppRegistry.registerComponent(appName, () => QFit);
---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {
+  Text,
+} from 'react-native';
+
+
+class QFit extends Component {
+  render() {
+    return (
+      <Text>Hello World</Text>
+    );
+  }
+};
+
+
+export default QFit;
